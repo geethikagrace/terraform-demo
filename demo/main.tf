eef59f5a895a52dd2b45ade92e8feee5cb9790fc
geethika <geethilka2219@gmail.com>
2023-06-13 04:48:12 -0700

    demo terraform

--- demo/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "demo-ec2" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "demo-ec2"
-  }
-}
---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "demo-ec2" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  security_groups = aws_security_group.demo-sg
+
+  tags = {
+    Name = "demo-ec2"
+  }
+}
+
+
+resource "aws_security_group" "demo-sg" {
+  name        = "${var.name}-sg"
+  description = "${var.name}-sg"
+  vpc_id      =  var.vpc_id
+
+  ingress {
+    description      = "demo"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+name = demo
+vpc_id = "vpc-0f5c2199ac552becb"
