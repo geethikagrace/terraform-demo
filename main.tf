2c7173028ab7928d3cd9985bfea5c7581ef5232b
geethika <geethilka2219@gmail.com>
2023-06-13 03:54:20 -0700

    demo terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "demo-ec2" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
-    Name = demo
+    Name = demo-ec2
   }
 }
